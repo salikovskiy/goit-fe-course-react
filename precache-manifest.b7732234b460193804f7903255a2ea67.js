7cffca44270a909da393f1ff8c6a4785d5484d0c
salikovskiy <ssalikovskiy@gmail.com>
2019-12-18 12:01:13 +0200

    Updates

---
+++ precache-manifest.b7732234b460193804f7903255a2ea67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebfa004208eb0b7d64a4651601fa9ad",
+    "url": "/goit-fe-course-react/index.html"
+  },
+  {
+    "revision": "113c1ee53ba27e85ea4b",
+    "url": "/goit-fe-course-react/static/css/main.2f670833.chunk.css"
+  },
+  {
+    "revision": "19f1aeb7bdacab4113cb",
+    "url": "/goit-fe-course-react/static/js/2.c5235a78.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-fe-course-react/static/js/2.c5235a78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "113c1ee53ba27e85ea4b",
+    "url": "/goit-fe-course-react/static/js/main.17763707.chunk.js"
+  },
+  {
+    "revision": "793b46e083d1d753ce6e",
+    "url": "/goit-fe-course-react/static/js/runtime-main.3ad2351c.js"
+  }
+]);
